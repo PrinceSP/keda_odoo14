eb189fdf16bf1c75aa2b891d5eafc48716365054
PrinceSP <princepasombaran@gmail.com>
2025-07-11 14:05:54 +0700

    test: create a test case to create and read material registration data

--- custom-addons/material_registration/tests/tests_material.py
+++ custom-addons/material_registration/tests/tests_material.py
@@ -1,7 +1,7 @@
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase, tagged
 
+@tagged('-standard', 'create_material')
 class TestMaterialRegistration(TransactionCase):
-
     def test_create_material(self):
         supplier = self.env['res.partner'].create({
             'name': 'Test Supplier',
@@ -15,6 +15,23 @@ class TestMaterialRegistration(TransactionCase):
             'buy_price': 120,
             'supplier_id': supplier.id
         })
-
+        self.assertEqual(material.supplier_id, supplier.id)
         self.assertEqual(material.buy_price, 120)
         self.assertEqual(material.supplier_id.name, 'Test Supplier')
+        
+    def test_read_material(self):
+        supplier = self.env['res.partner'].create({
+            'name': 'Test Supplier',
+            'is_supplier': True
+        })
+
+        material = self.env['material.registration'].create({
+            'code': 'MAT-001',
+            'name': 'Premium Fabric',
+            'material_type': 'fabric',
+            'buy_price': 120,
+            'supplier_id': supplier.id
+        })
+        self.assertEqual(supplier.name, 'Test Project')
+        self.assertEqual(material.name, 'Premium Fabric')
+
